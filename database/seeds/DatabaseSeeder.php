1b1ed23b662702cc3c32c6dd440da6d542181a5a
boryeira <jmanuel.jorquera@gmail.com>
2020-04-16 13:40:18 -0400

    seed to 50

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategoriesTableSeeder::class);
-        factory(App\Product::class, 10)->create();
-        factory(App\User::class, 10)->create();
+        factory(App\Product::class, 50)->create();
+        //factory(App\User::class, 10)->create();
 
 
 
